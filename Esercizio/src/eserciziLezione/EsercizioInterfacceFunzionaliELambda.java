816edf2a5ab70d9dfc929a4dd14ad83daaa9f942
mak-1 <38510654+mak-1@users.noreply.github.com>
2023-06-01 13:00:51 +0200

    Aggiunta di stringe nelle print

--- Esercizio/src/eserciziLezione/EsercizioInterfacceFunzionaliELambda.java
+++ Esercizio/src/eserciziLezione/EsercizioInterfacceFunzionaliELambda.java
@@ -10,11 +10,11 @@ import java.util.function.Supplier;
 public class EsercizioInterfacceFunzionaliELambda {
     public static void main(String[] args) {
         List<Integer> listaNumeri= Arrays.asList(1,2,3,4,5,6);
-        Consumer<Integer> consumer= (x) -> System.out.println(x);
+        Consumer<Integer> consumer= (x) -> System.out.println("Elemento della lista "+x);
         listaNumeri.forEach(consumer);
 
         List<String> listaStringhe = Arrays.asList("Mario","Pippo","Luigi","Davide");
-        Consumer<String> consumer2= (x) -> {if(x.startsWith("L")) System.out.println(x);};
+        Consumer<String> consumer2= (x) -> {if(x.startsWith("L")) System.out.println("Nome che comincia con la lettera L maiuscola nell'elenco " +x);};
         listaStringhe.forEach(consumer2);
 
 
@@ -23,7 +23,7 @@ public class EsercizioInterfacceFunzionaliELambda {
            return rand.nextInt(1,11);};
 
         int intero = interoCasuale.get();
-        System.out.println(intero);
+        System.out.println("Numero casuale "+intero);
     }
 
 
